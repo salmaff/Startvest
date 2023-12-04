f94c32b6ef48c90f4bc919de5445d3cdcb52c68f
rrtxt <rizfiferdian@gmail.com>
2023-12-04 11:23:00 +0700

    Add Notification Pop Up

--- resources/views/components/auth-profile.blade.php
+++ resources/views/components/auth-profile.blade.php
@@ -14,4 +14,38 @@
             <iconify-icon icon="gridicons:dropdown" style="color: #ff872e;" width="40" height="40"></iconify-icon>
         </div>
     </div>
+    <div id="notification">
+        <div class="notif-header">
+            My Notification
+        </div>
+        <div class="notif-content">
+            <div class="notif-modal">
+                <div class="notif-title">
+                    Notifikasi Keuangan
+                </div>
+                <div class="notif-desc">
+                    Pembayaran investasi 745774577457 <br>
+                    berhasil dilakukan. Cek rinciannya di...
+                </div>
+            </div>
+            <div class="notif-modal">
+                <div class="notif-title">
+                    Notifikasi Akun
+                </div>
+                <div class="notif-desc">
+                    Foto profil Anda berhasil di-update. <br>
+                    Lihat di sini.
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script>
+    const notifBtn = document.querySelector('#bell-icon')
+    const notif = document.querySelector('#notification')
+    notifBtn.addEventListener('click', (e) => {
+        console.log(e);
+        notif.classList.toggle('active')
+    })
+</script>
