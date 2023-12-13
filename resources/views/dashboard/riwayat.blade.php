6a86f35ce6e97243203d7ba5c05552bdb147ca0f
rrtxt <rizfiferdian@gmail.com>
2023-12-13 17:11:10 +0700

    Add Sort by name functionality

--- resources/views/dashboard/riwayat.blade.php
+++ resources/views/dashboard/riwayat.blade.php
@@ -44,7 +44,7 @@
                 ></iconify-icon>
             </div>
         </div>
-        <div class="filter-name">
+        <!-- <div class="filter-name">
             <div class="startup-name">Hire</div>
             <div class="drop-icon">
                 <iconify-icon
@@ -91,7 +91,7 @@
                     "
                 ></iconify-icon>
             </div>
-        </div>
+        </div> -->
     </div>
     <div class="wrapper-container">
         <div class="wrapper">
@@ -154,13 +154,38 @@
             <h3>Returns</h3>
             <h2>- Rp 2.000.000</h2>
         </div>
+        <div class="wrapper">
+            <img
+                src="{{ asset('static/image/cak/logo/logo-fire.png') }}"
+                alt=""
+            />
+            <div class="text">
+                <p>Aug 2023</p>
+                <h2>F!ame</h2>
+            </div>
+            <h3>Returns</h3>
+            <h2>- Rp 2.000.000</h2>
+        </div>
     </div>
 </div>
 
 @endsection @section('js')
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 <script>
+    const filterBtn = document.querySelector(".filter");
+    const filterCard = document.querySelector("#filter-card");
     const histories = document.querySelectorAll(".text > h2");
+
+    //Filter card dropdown
+    filterCard.style.display = "none";
+
+    filterBtn.addEventListener("click", (e) => {
+        filterCard.style.display =
+            filterCard.style.display === "none" ||
+            filterCard.style.display === ""
+                ? "block"
+                : "none";
+    });
 
     //Get All Start Up Name
     var startups = [];
@@ -170,6 +195,67 @@
         }
     });
 
-    console.log(startups);
+    startups.forEach((ele) => {
+        var dynamicContent = document.createElement("div");
+        dynamicContent.innerHTML = `<div class="filter-name">
+            <div class="startup-name">${ele}</div>
+            <div class="drop-icon">
+                <iconify-icon
+                    icon="fa6-solid:v"
+                    width="10"
+                    height="12"
+                    style="
+                        color: black;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                    "
+                ></iconify-icon>
+            </div>
+        </div>`;
+        filterCard.appendChild(dynamicContent);
+    });
+
+    //Filter section
+    const filterName = document.querySelectorAll(".filter-name");
+    const wrapperContainer = document.querySelector(".wrapper-container");
+    const startupsCard = document.querySelectorAll(
+        ".wrapper-container > .wrapper"
+    );
+    var selectedCompany = "";
+
+    console.log(startupsCard);
+
+    filterName.forEach((ele) => {
+        ele.addEventListener("click", (e) => {
+            selectedCompany = ele.querySelector(".startup-name").innerHTML;
+            //console.log(selectedCompany);
+
+            //startupsCard.forEach((ele) => {
+            // console.log(ele.querySelector(".text > h2").innerHTML);
+            //});
+            const isAll = selectedCompany === "Filter" ? true : false;
+            const sorted = sortByName(startupsCard, selectedCompany, isAll);
+            wrapperContainer.innerHTML = "";
+            sorted.forEach((ele) => {
+                wrapperContainer.appendChild(ele);
+            });
+        });
+    });
+
+    function sortByName(startups, target, isAll) {
+        var result = [];
+        if (isAll) {
+            result = startups;
+            return result;
+        }
+        startups.forEach((startup) => {
+            if (startup.querySelector(".text > h2").innerHTML === target) {
+                result.push(startup);
+            }
+        });
+
+        return result;
+    }
 </script>
 @endsection
